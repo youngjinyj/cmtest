1f3e1e2597e14c06f76180a9c0e12a61b9ed434c
youngjinyj <127479383+youngjinyj@users.noreply.github.com>
2023-05-15 17:07:13 +0900

    Add files via upload

---
+++ Printna.java
@@ -0,0 +1,29 @@
+package fileInterface;
+
+import java.util.Calendar;
+import java.util.Locale;
+
+public class Printna {
+
+	public static void main(String[] args) {
+		long n = 461012;
+		System.out.format("%d%n",n);
+		System.out.format("%08d%n",n);
+		System.out.format("%+8d%n",n);
+		System.out.format("%,8d%n",n);
+		System.out.format("%+,8d%n%n",n);
+		
+		double pi = Math.PI;
+		System.out.format("%f%n",pi);
+		System.out.format("%.3f%n",pi);
+		System.out.format("%10.3f%n",pi);
+		System.out.format("%-10.4f%n%n",pi);
+		System.out.format(Locale.FRANCE,"%-10.4f%n%n",pi);
+		
+		Calendar c = Calendar.getInstance();
+		System.out.format("%tB%te,%tY%n",c,c,c);
+		System.out.format("%tl:%tM,%tp%n",c,c,c);
+		System.out.format("%tD%n",c);
+	}
+
+}
